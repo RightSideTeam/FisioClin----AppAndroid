45ba55547cae6aad504d1ebc6bef58d57e46e191
Matheus Lima <matheusldasilva20088@gmail.com>
2019-10-10 03:49:16 -0300

    alteração na informação no dialog de nova consulta

--- app/src/main/java/com/rightside/fisioclin/controller/NovaConsultaController.java
+++ app/src/main/java/com/rightside/fisioclin/controller/NovaConsultaController.java
@@ -2,6 +2,7 @@ package com.rightside.fisioclin.controller;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.view.Gravity;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
@@ -15,8 +16,13 @@ public class NovaConsultaController {
 
     public static void alertaDeNovaConsulta(final FragmentActivity activity, final Horario horario) {
         final AlertDialog.Builder alerta = new AlertDialog.Builder(activity);
+        final TextView textView = new TextView(activity.getApplicationContext());
+        textView.setText(horario.getDiaSemana() +" - "+ horario.getData());
+        textView.setTextSize(15);
+        textView.setGravity(Gravity.CENTER);
+        alerta.setView(textView);
 
-        alerta.setTitle("Deseja marcar uma consulta?").setPositiveButton("Avançar", new DialogInterface.OnClickListener() {
+        alerta.setTitle("Deseja marcar uma consulta para:").setPositiveButton("Avançar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 MarcarConsultaDialogFragment.novaInstancia(horario).show(activity.getSupportFragmentManager(), "novaConsulta");
